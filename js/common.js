b9e0c5b47129ad0529457e8bb0b9e43b2eed9bd6
unknown <HM801372@ntt-adgrp.net>
2021-05-06 13:58:07 +0900

    fin

--- js/common.js
+++ js/common.js
@@ -3,24 +3,42 @@
 	set = function() {
 		
 		$('<div class="back">').prependTo('body > header , article');		
-		$('article , body > header').addClass('contents');	
+		$('article , body > header').addClass('contents');
+	},	
+	
+	size = function() {
+		
+		if ($(window).width() < 600) {
+			$('h2 img').attr('src' , 'img/logo2.png');
+		} else {
+			$('h2 img').attr('src' , 'img/logo.png');		
+		}
+		$('iframe').height(($('iframe').width() / 16) * 9);
 	},	
 		
 	// SmoothScroll
 	smoothScroll = function() {
 		
-		$('a[href^="#"]').on('click' , function(){
-			var href= $(this).attr("href"),
-				target = $(href == "#" || href == "" ? 'html' : href),
-				position = target.offset().top;
-			
-			$("html , body").animate({scrollTop:position}, 500, "swing");
-			return false;
-		});
-		
 		var boxTop			= new Array,
 			current			= -1,
-			startPosition	= 0;
+			startPosition	= 0,
+			scrollswitch 	= 0;
+		
+		$('a[href^="#"]').click(function(ev) {
+			var href= $(this).attr("href"),
+				target = $(href == "#" || href == "" ? 'html' : href),
+				position = target.offset().top + 1;
+			
+			scrollswitch = 1;
+			
+			$("html , body").animate({scrollTop:position}, 500, "swing" , function() {
+				$("html , body").stop(true,true);
+				setTimeout(function(){
+					scrollswitch = 0;
+				},1000);
+			});
+			ev.preventDefault();
+		});
 		
 		//各要素の位置
 		$(window).on("load resize", function(){
@@ -36,12 +54,11 @@
 			var scrollPosition	= $(window).scrollTop();
 				
 			for (var i = boxTop.length - 1 ; i >= 0; i--) {
-				if (scrollPosition > boxTop[i]) {
+				if (scrollPosition >= boxTop[i]) {
 					changeBox(i);
 					break;
-				} else if (scrollPosition <= boxTop[0]) {
+				} else if (scrollPosition < boxTop[0]) {
 					changeBox(-1);
-					$("html , body").stop(true,false);
 				}
 			}
 
@@ -58,32 +75,46 @@
 		});
 		
 		//ナビの処理
-		function changeBox(secNum) {		
-				
-			var scrollBottom = $(window).scrollTop() + $(window).height();
+		function changeBox(secNum) {
 			
-			if (secNum != current) {
+			/*if($(window).width() > 1200) {
+				
+				var scrollBottom = $(window).scrollTop() + $(window).height();
 
-				if (secNum < current && secNum != 0) {
-					$("html , body").animate({scrollTop: boxTop[current] - $(window).height()}, 500, "swing");
-				}
-				
-				current = secNum;
-				secNum2 = secNum + 1;//HTML順序用
-				$('main nav li').removeClass('on');
-				$('main nav li:nth-child(' + secNum2 +')').addClass('on');
-				
-			} else {
-				
-				if (scrollBottom >= boxTop[current + 1] && startPosition < $(window).scrollTop()) {
-					$("html , body").stop(true,false).animate({scrollTop: boxTop[current + 1]}, 50, "swing");
-				
+				if (secNum != current && scrollswitch == 0) {
+
+					if (secNum < current) {
+						$("html , body").animate({scrollTop: boxTop[current] - $(window).height() - 1}, 500, "swing");
+					}
+
 					current = secNum;
 					secNum2 = secNum + 1;//HTML順序用
 					$('main nav li').removeClass('on');
 					$('main nav li:nth-child(' + secNum2 +')').addClass('on');
+
+				} else {
+
+					if (scrollBottom > boxTop[current + 1] && startPosition < $(window).scrollTop() && secNum != 0 && scrollswitch == 0) {
+						$("html , body").stop(true,false).animate({scrollTop: boxTop[current + 1] + 1}, 50, "swing");
+
+						current = secNum;
+						secNum2 = secNum + 1;//HTML順序用
+						$('main nav li').removeClass('on');
+						$('main nav li:nth-child(' + secNum2 +')').addClass('on');
+					}
 				}
-			}
+				
+			} else {
+				current = secNum;
+				secNum2 = secNum + 1;//HTML順序用
+				$('main nav li').removeClass('on');
+				$('main nav li:nth-child(' + secNum2 +')').addClass('on');
+			}*/
+			
+			current = secNum;
+			secNum2 = secNum + 1;//HTML順序用
+			$('main nav li').removeClass('on');
+			$('main nav li:nth-child(' + secNum2 +')').addClass('on');
 		};
 	},
 	
@@ -91,18 +122,10 @@
 	side = function() {
 		$('main nav a').append('●');
 		
-		$('main nav').hover(function() {
-			$('body').append('<div id="modal"></div>');
-			$('span' , this).fadeIn('slow').css('display' , 'inline');
-		},function() {
-			$('div#modal').remove();
-			$('span' , this).fadeOut('slow');
-		});
-		
-		$('main nav').on('touchstart', function(){
-			$('body').append('<div id="modal"></div>');
-			$('span',this).fadeIn('slow');
-    	}).on('touchend', function(){
+		$('main nav').on('mouseenter touchstart', function(){
+			$('main').append('<div id="modal"></div>');
+			$('span',this).fadeIn('fast').css('display' , 'inline');
+    	}).on('mouseleave touchend', function(){
         	$('div#modal').remove();
 			$('span',this).fadeOut('slow');
     	});
@@ -112,6 +135,11 @@
 		set();
 		smoothScroll();
 		side();
+		size();
+	});
+	
+	$(window).resize(function() {
+		size();
 	});
 	
 })($);
